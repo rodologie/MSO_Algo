cce4c6007114710b5949fa00f3afd05556594e37
rodologie <91213039+rodologie@users.noreply.github.com>
2023-03-09 17:58:34 +0100

    bofbof

---
+++ TP1/Josephus.py
@@ -0,0 +1,87 @@
+# On crée une classe qui représente un Noeud dans une liste chainée
+class Noeud():
+    def __init__(self, valeur):
+        self.valeur = valeur
+        self.suivant = None
+    
+    def __str__(self):
+        return str(self.valeur)
+
+# On crée une classe qui représente une liste chainée
+class ListeChainee():
+    def __init__(self):
+        self.tete = None
+        self.queue = None
+        self.taille = 0
+
+    def __str__(self):
+        chaine = ""
+        courant = self.tete
+        while courant != None:
+            chaine += str(courant) + " "
+            courant = courant.suivant
+        print(chaine)
+    
+    def inserer(self, valeur, position):
+        nouveau = Noeud(valeur)
+        if position == 0:
+            nouveau.suivant = self.tete
+            self.tete = nouveau
+        else:
+            courant = self.tete
+            for i in range(position - 1):
+                courant = courant.suivant
+            nouveau.suivant = courant.suivant
+            courant.suivant = nouveau
+        self.taille += 1
+    
+    def supprimer(self, position):
+        if position == 0:
+            self.tete = self.tete.suivant
+        else:
+            courant = self.tete
+            for i in range(position - 1):
+                courant = courant.suivant
+            courant.suivant = courant.suivant.suivant
+        self.taille -= 1
+
+    def rechercher(self, valeur):
+        courant = self.tete
+        position = 0
+        while courant != None:
+            if courant.valeur == valeur:
+                return courant
+            courant = courant.suivant
+            position += 1
+        return -1
+    
+    def taille(self):
+        return self.taille
+    
+    def estVide(self):
+        return self.taille == 0
+    
+
+
+#chat gpt
+
+
+#def josephus(n, k):
+    # Créer une liste circulaire avec n éléments
+#    circle = list(range(1, n+1))
+
+    # Index du soldat à tuer
+#    idx = 0
+
+    # Tuer les soldats jusqu'à ce qu'il n'en reste qu'un
+#        # Avancer de k soldats dans le sens horaire
+#        idx = (idx + k - 1) % len(circle)
+#        print(circle[idx])
+
+        # Tuer le soldat à l'index courant
+#        circle.pop(idx)
+
+    # Le dernier soldat survivant est le premier élément de la liste
+#    return circle[0]
+
+#print(josephus(15,4))
