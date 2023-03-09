a9f9a9598f6154aa9760dafb94bc2a4031298fc7
rodologie <91213039+rodologie@users.noreply.github.com>
2023-03-09 11:14:46 +0100

    supprimer() à améliorer

---
+++ TP1/Exo3.py
@@ -0,0 +1,60 @@
+# On crée une classe qui représente un Noeud dans une liste chainée
+class Noeud:
+    def __init__(self, valeur):
+        self.valeur = valeur
+        self.suivant = None
+    
+    def __str__(self):
+        return str(self.valeur)
+
+# On crée une classe qui représente une liste chainée
+class pile():
+    def __init__(self):
+        self.tete = None
+        self.queue = None
+        self.taille = 0
+
+    def __str__(self):
+        chaine = ""
+        courant = self.tete
+        while courant != None:
+            chaine += str(courant) + " "
+            courant = courant.suivant
+        print(chaine)
+    
+    def inserer(self, valeur, position):
+        nouveau = Noeud(valeur)
+        courant = self.tete
+        nouveau.suivant = None
+        courant.suivant = nouveau
+        self.taille += 1
+    
+    def supprimer(self,position):
+        courant = self.tete
+        for i in range(position - 1):
+            courant = courant.suivant
+        courant.suivant = courant.suivant.suivant
+        self.taille -= 1
+
+    def rechercher(self, valeur):
+        courant = self.tete
+        position = 0
+        while courant != None:
+            if courant.valeur == valeur:
+                return position
+            courant = courant.suivant
+            position += 1
+        return -1
+    
+    def taille(self):
+        return self.taille
+    
+    def estVide(self):
+        return self.taille == 0
+    
+maLst = pile()
+maLst.inserer(5,0)
+maLst.inserer(1,1)
+maLst.inserer(2,2)
+maLst.supprimer(2)
+maLst.__str__()
