1bdf50a78dab885aa2df8790f13e18ab303e9d93
rodologie <91213039+rodologie@users.noreply.github.com>
2023-03-24 14:37:02 +0100

    fin

--- TP3/Exo6.py
+++ TP3/Exo6.py
@@ -9,5 +9,21 @@
 # Elle est en moyenne de (3log(n)+1)/2.
 
 
-def dicho():
-    return 0
+def dicho(list,lst,k):
+    if lst[len(lst)//2] > k:
+        if k<lst[0]:
+            print("impossible")
+            return -1
+        else:
+            dicho(list,lst[:len(lst)//2],k)
+    elif lst[len(lst)//2] < k:
+        if k>lst[-1]:
+            print("impossible")
+        else:
+            dicho(list, lst[len(lst)//2:],k)
+    elif lst[len(lst)//2] == k:
+        print(list.index(k))
+
+
+lst = [2,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+dicho(lst,lst,7)
